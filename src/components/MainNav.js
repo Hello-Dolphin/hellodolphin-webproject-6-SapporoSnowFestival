681b703521a2d80fb31928fb496c49d534ad2774
ModHanami <modhanami@hotmail.co.th>
2020-11-12 21:16:39 +0700

    Add MainNav and mali bold

---
+++ src/components/MainNav.js
@@ -0,0 +1,83 @@
+import styled from 'styled-components';
+import NavItem from './NavItem';
+import logoWhite from '../assets/images/logo/logo-white.png'
+import logoDark from '../assets/images/logo/logo-dark.png'
+
+const NavLogo = styled.a.attrs(props => ({
+  href: props.to,
+}))`
+  position: absolute;
+  left: 0;
+  & img {
+    width: 84px;
+    height: 84px;
+  }
+`;
+
+const NavBar = styled.div`
+  position: sticky;
+  top: 0;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  height: 66px;
+  z-index: 100;
+  background-color: var(--color-secondary);
+  & li {
+    height: 100%;
+    font-weight: bold;
+    color: white;
+  }
+  & a, .dummy {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 0 3vmax;
+    height: 100%;
+    font-size: 1.125em;
+    text-decoration: none;
+    color: unset;
+  }
+  & a:active, & a:hover {
+    background-color: var(--color-primary);
+    color: var(--color-secondary);
+  }
+  & .nav-logo {
+    padding: 0 1em;
+  }
+`;
+
+const NavMenu = styled.ul`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  list-style: none;
+  
+`;
+
+function MainNav() {
+	return (
+    <NavBar>
+      <NavLogo
+        to="#Home"
+        className="nav-logo"
+        onMouseEnter={() => document.getElementById("logo").setAttribute("src", logoDark)}
+        onMouseLeave={() => document.getElementById("logo").setAttribute("src", logoWhite)}
+      >
+        <img src={logoWhite} alt="Brand logo" id="logo"/>
+      </NavLogo>
+      <NavMenu>
+        <NavItem to="#History" text="ความเป็นมา" />
+        <NavItem to="#Locations" text="สถานที่" />
+        <NavItem to="#Activities" text="กิจกรรม" />
+        <NavItem to="#Outfits" text="การแต่งกาย" />
+        <NavItem to="#About" text="เกี่ยวกับ" />
+      </NavMenu>
+      {/* <div className="dummy"></div> */}
+    </NavBar>
+	)
+}
+
+export default MainNav;
