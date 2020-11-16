3725e94d832bd8713044224c42f18dcd6fba5536
ModHanami <modhanami@hotmail.co.th>
2020-11-16 18:25:57 +0700

    Comment out jumToSection

--- src/App.js
+++ src/App.js
@@ -18,11 +18,11 @@ import { Waypoint } from 'react-waypoint';
 const setAppHeightThrottled = throttle(300, (callback) => {
   callback(`${document.body.scrollHeight}px`)
 });
-const jumpToSection = debounce(1, (section) => {
-  let target = document.querySelector(`#${section}`);
-  if (!target) return null;
-  target.scrollIntoView();
-})
+// const jumpToSection = debounce(1, (section) => {
+//   let target = document.querySelector(`#${section}`);
+//   if (!target) return null;
+//   target.scrollIntoView();
+// })
 
 let setCurrentSectionDebounced;
 
